1dad1c9ebf9bd222d54e4e1cf0177e5eb6410641
Renatofujimoto <renatofujimoto2@gmail.com>
2024-05-05 19:32:39 -0300

    chore: update node api

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
 import http from "node:http"
 
+const users = [];
+
 const server = http.createServer((req, res) => {
-    return res.end("Hello World")
+
+    const { method, url } = req
+
+    if (method === "GET" && url === "/users") {
+        return res
+            .setHeader("Content-type", "application/json")
+            .end(JSON.stringify(users))
+    }
+
+    if (method === "POST" && url === "/users") {
+        users.push({
+            id: 1,
+            name: "Renato",
+            email: "renatofujimoto@gmai.com"
+        })
+
+        res.writeHead(201).end("Usuário Criado")
+    }
+
+    return res.writeHead(404).end("Rota não encontrada")
 })
 
 server.listen(4000)
